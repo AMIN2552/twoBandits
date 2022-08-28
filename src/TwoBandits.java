9cfb13db25f072074801c2142526333ee8e0383f
user <amin7061205@gmail.com>
2022-08-28 15:26:22 +0300

    Initial commit

---
+++ src/TwoBandits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TwoBandits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int c = 10 - a;
+         int d = 10 - b;
+        System.out.print(c);
+        System.out.print( " " + d );
+        
+    }
+}
